961f776282b520b69ef82cb6f3c0e1bd856cfb97
yellow013 <wk9988@gmail.com>
2019-10-26 12:22:52 +0800

    update common actor

---
+++ src/main/java/io/ffreedom/actors/CommonActor.java
@@ -0,0 +1,13 @@
+package io.ffreedom.actors;
+
+import akka.actor.AbstractActor;
+
+public class CommonActor extends AbstractActor{
+
+	@Override
+	public Receive createReceive() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
