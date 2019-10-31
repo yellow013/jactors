ccd21789b3f6a0d80b461d45b69fafef1c6a6063
yellow013 <wk9988@gmail.com>
2019-10-31 20:14:25 +0800

    update test

--- src/test/java/io/ffreedom/actors/UserActor.java
+++ src/test/java/io/ffreedom/actors/UserActor.java
@@ -3,11 +3,11 @@ package io.ffreedom.actors;
 import akka.actor.Props;
 import io.ffreedom.actors.reference.SingleGenericActor;
 
-public class UserLoginActor extends SingleGenericActor<User> {
+public class UserActor extends SingleGenericActor<User> {
 //    private Object parameters;
 
 	public static Props props() {
-		return Props.create(UserLoginActor.class, UserLoginActor::new);
+		return Props.create(UserActor.class, UserActor::new);
 	}
 
 	// 如果有构造参数, 便按照如下方式构造 Props 即可
@@ -20,13 +20,13 @@ public class UserLoginActor extends SingleGenericActor<User> {
 //    }
 
 	@Override
-	protected void handle(User user) {
+	protected void onEvent(User user) {
 		System.out.println("received user -> userId==[" + user.getUserId() + "], userName==[" + user.getUserName()
 				+ "], age==[" + user.getAge() + "]");
 	}
 
 	@Override
-	protected Class<User> getType() {
+	protected Class<User> eventType() {
 		return User.class;
 	}
 
--- src/test/java/io/ffreedom/actors/UserActorTest.java
+++ src/test/java/io/ffreedom/actors/UserActorTest.java
@@ -3,17 +3,19 @@ package io.ffreedom.actors;
 import akka.actor.ActorRef;
 import io.ffreedom.common.thread.ThreadUtil;
 
-public class ActorTest {
+public class UserActorTest {
 
 	public static void main(String[] args) {
 
-		ActorRef userLoginActor = ActorSysRoot.INSTANCE.actorAtRootOf(UserLoginActor.props(),
-				UserLoginActor.class.getName());
+		ActorRef userLoginActor = ActorSysRoot.INSTANCE.actorAtRootOf(UserActor.props(),
+				UserActor.class.getName());
 		
 		userLoginActor.tell(new User(1, "user1", 10), ActorRef.noSender());
 		userLoginActor.tell("dafaf", ActorRef.noSender());
 		
-		ThreadUtil.sleep(1000);
+		
+		
+		ThreadUtil.sleep(3000);
 		ActorSysRoot.INSTANCE.getActorSystem().terminate();
 
 	}
