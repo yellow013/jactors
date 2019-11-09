3cf93c3ddd11f7a12338b3d000f3a008d4a4dc71
yellow013 <wk9988@gmail.com>
2019-11-09 13:56:54 +0800

    update add sample

--- src/test/java/io/ffreedom/actors/sample/supervision/Main.java
+++ src/test/java/io/ffreedom/actors/sample/supervision/Main.java
@@ -15,24 +15,19 @@ import static akka.japi.Util.classTag;
 
 public class Main {
 
-  public static void main(String[] args) throws Exception {
-    ActorSystem system = ActorSystem.create("calculator-system");
-    ActorRef calculatorService =
-      system.actorOf(Props.create(ArithmeticService.class), "arithmetic-service");
+	public static void main(String[] args) throws Exception {
+		ActorSystem system = ActorSystem.create("calculator-system");
+		ActorRef calculatorService = system.actorOf(Props.create(ArithmeticService.class), "arithmetic-service");
 
-    // (3 + 5) / (2 * (1 + 1))
-    Expression task = new Divide(
-      new Add(new Const(3), new Const(5)),
-      new Multiply(
-        new Const(2),
-        new Add(new Const(1), new Const(1))
-      )
-    );
+		// (3 + 5) / (2 * (1 + 1))
+		Expression task = new Divide(new Add(new Const(3), new Const(5)),
+				new Multiply(new Const(2), new Add(new Const(1), new Const(1))));
 
-    FiniteDuration duration = Duration.create(1, TimeUnit.SECONDS);
-    Integer result = Await.result(ask(calculatorService, task, new Timeout(duration)).mapTo(classTag(Integer.class)), duration);
-    System.out.println("Got result: " + result);
+		FiniteDuration duration = Duration.create(1, TimeUnit.SECONDS);
+		Integer result = Await
+				.result(ask(calculatorService, task, new Timeout(duration)).mapTo(classTag(Integer.class)), duration);
+		System.out.println("Got result: " + result);
 
-    Await.ready(system.terminate(), Duration.Inf());
-  }
+		Await.ready(system.terminate(), Duration.Inf());
+	}
 }
